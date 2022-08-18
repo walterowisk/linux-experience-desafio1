e1c5d36b269c0797ab705f818b5b5d6d27a54548
Walter Silva <walter4web@gmail.com>
2022-08-18 10:46:14 -0300

    setting user permissions on directories

--- iac1.sh
+++ iac1.sh
@@ -5,23 +5,24 @@
 # Script criado por Walter J. Silva
 # GitHub: /walterowisk
 
-echo "Criando os diretórios"
+echo "Script IaC iniciado!!"
+echo "Criando os diretórios..."
 
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
 
-echo "Criando os grupos"
+echo "Criando os grupos..."
 
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "Criando os usuários e atribuindo os grupos determinados"
+echo "Criando os usuários e atribuindo os grupos determinados..."
 
 useradd carlos -c "Carlos Villagran" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_ADM
-useradd Maria -c "Maria do Bairro" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_ADM
+useradd maria -c "Maria do Bairro" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_ADM
 useradd joao -c "João Canalha" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_ADM
 
 useradd debora -c "Débora Nascimento" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_VEN
@@ -32,8 +33,17 @@ useradd josefina -c "Josefininha" -s /bin/bash -m -p $(openssl passwd -6 102030)
 useradd amanda -c "Amanda Carvalho" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_SEC
 useradd rogerio -c "Rogério Cenior" -s /bin/bash -m -p $(openssl passwd -6 102030) -G GRP_SEC
 
-echo "Definindo os donos e grupos dos diretórios"
+echo "Definindo os donos e grupos dos diretórios..."
 
 chown root:GRP_ADM /adm/
 chown root:GRP_VEN /ven/
 chown root:GRP_SEC /sec/
+
+echo "Definindo as permissões de usuário nos diretórios..."
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+
+echo "Script IaC finalizado!!"
